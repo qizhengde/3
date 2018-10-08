692a9d1387f23852e0a2482112404f6d5da3ba17
qizhengde <43947900+qizhengde@users.noreply.github.com>
2018-10-08 14:51:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/think/helloword/HellowordActivity.java
@@ -0,0 +1,13 @@
+package com.example.think.helloword;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HellowordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hello_word_layout);
+    }
+}
